af56f2d89cc0483dbb33c13379efadf43de11abf
scalliontor <vuhunganh0301@gmail.com>
2026-05-15 14:22:00 +0700

    feat: add frontend tests - RBAC, ProtectedRoute, and page components

--- tests/e2e/03_room.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source "$(dirname "$0")/helpers.sh"
-echo "Running 03_room.sh..."
-
--- tests/e2e/07_error_handling.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source "$(dirname "$0")/helpers.sh"
-echo "Running 07_error_handling.sh..."
-
